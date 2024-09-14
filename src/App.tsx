084cd610a4e2711049ac819290602c9dad8de90c
Matias Agustin Area <matuarea@gmail.com>
2024-09-14 13:28:14 -0300

    Add Tailwind CSS and PostCSS configuration; refactor App component with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'leaflet/dist/leaflet.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import { About } from './routes/about'
+import { Contact } from './routes/contact'
+import { Home } from './routes/home'
+import { Layout } from './components/Layout/Layout'
 
-export default App
+export const App = () => (
+	<Router>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path="about" element={<About />} />
+				<Route path="contact" element={<Contact />} />
+			</Route>
+		</Routes>
+	</Router>
+)
