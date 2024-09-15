fcbbc17c29fc00c4e91f2bda9a957c42d65ead6a
Matias Agustin Area <matuarea@gmail.com>
2024-09-15 14:11:48 -0300

    Refactor App component to load initial employee and map point data; update Home component to manage state and render data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import 'leaflet/dist/leaflet.css'
 
+import React, { useEffect } from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
-import { About } from './routes/about'
-import { Contact } from './routes/contact'
+import EmployeeService from './services/EmployeeService'
 import { Home } from './routes/home'
 import { Layout } from './components/Layout/Layout'
+import MapPointsService from './services/MapPointService'
 
-export const App = () => (
-	<Router>
-		<Routes>
-			<Route path="/" element={<Layout />}>
-				<Route index element={<Home />} />
-				<Route path="about" element={<About />} />
-				<Route path="contact" element={<Contact />} />
-			</Route>
-		</Routes>
-	</Router>
-)
+const App: React.FC = () => {
+	useEffect(() => {
+		EmployeeService.loadInitialEmployeesFromFile()
+		MapPointsService.loadInitialPointsFromFile()
+	}, [])
+
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					{/* <Route path="about" element={<About />} /> */}
+					{/* <Route path="contact" element={<Contact />} /> */}
+				</Route>
+			</Routes>
+		</Router>
+	)
+}
+
+export default App
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,17 +1,55 @@
-import { EmployeeComponent } from '../components/Employee/EmployeeComponent'
-import { MapComponent } from '../components/Map/MapComponent'
+import { createColorArray, groupMapPointsByColor } from '../utils/functions'
+import { useEffect, useState } from 'react'
+
+import { Employee } from '../types/Employee.types'
+import EmployeeComponent from '../components/Employee/EmployeeComponent'
+import EmployeeService from '../services/EmployeeService'
+import MapComponent from '../components/Map/MapComponent'
+import { MapPoint } from '../types/MapPoint.types'
+import MapPointService from '../services/MapPointService'
 import TableComponent from '../components/Table/TableComponent'
 
-export const Home = () => (
-	<div className="p-4">
-		<div className="flex justify-between items-stretch w-full">
-			<EmployeeComponent />
-			<div className="h-96 w-4/5 ">
-				<MapComponent />
+export const Home: React.FC = () => {
+	const [employees, setEmployees] = useState<Employee[]>([])
+	const [mapPoints, setMapPoints] = useState<MapPoint[]>([])
+
+	useEffect(() => {
+		try {
+			const employees = EmployeeService.getEmployees()
+			setEmployees(employees)
+		} catch (e) {
+			console.error(e)
+		}
+	}, [])
+
+	useEffect(() => {
+		try {
+			const mapPoints = MapPointService.getPoints()
+			setMapPoints(mapPoints)
+		} catch (e) {
+			console.error(e)
+		}
+	}, [])
+
+	const colorArray = createColorArray(employees)
+	const mapPointsByColor = groupMapPointsByColor(mapPoints, employees)
+
+	console.log('COLOR ARRAY', colorArray)
+	console.log('MAP POINT', mapPointsByColor)
+
+	return (
+		<div className="p-4">
+			<div className="flex flex-wrap-reverse justify-between items-stretch w-full">
+				<div className="w-full md:w-1/6 mb-4">
+					<EmployeeComponent employees={employees} />
+				</div>
+				<div className="w-full md:w-4/5 h-96 mb-4">
+					<MapComponent points={mapPointsByColor} />
+				</div>
+			</div>
+			<div className="flex flex-col justify-center items-center  w-full py-4">
+				<TableComponent data={mapPoints} />
 			</div>
 		</div>
-		<div className="container mx-auto w-full py-4">
-			<TableComponent />
-		</div>
-	</div>
-)
+	)
+}
