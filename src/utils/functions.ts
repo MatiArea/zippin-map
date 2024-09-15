3065f8c2c248be711aabdc59b516337c9931f40e
Matias Agustin Area <matuarea@gmail.com>
2024-09-15 14:13:23 -0300

    Add utility functions for grouping map points and creating color arrays

---
+++ src/utils/functions.ts
@@ -0,0 +1,39 @@
+import { Employee } from '../types/Employee.types'
+import { MapPoint } from '../types/MapPoint.types'
+
+type ColorMap = {
+	[color: string]: number[]
+}
+
+type GroupedMapPoints = {
+	[color: string]: MapPoint[]
+}
+
+export const createColorArray = (employees: Employee[]): ColorMap => {
+	return employees.reduce((map: ColorMap, employee) => {
+		if (!map[employee.color]) {
+			map[employee.color] = []
+		}
+		map[employee.color].push(employee.id)
+		return map
+	}, {} as ColorMap)
+}
+
+export const groupMapPointsByColor = (
+	mapPoints: MapPoint[],
+	employees: Employee[],
+): GroupedMapPoints => {
+	return mapPoints.reduce((result: GroupedMapPoints, point) => {
+		const employee = employees.find((emp) => emp.id === point.assignedDriverId)
+
+		if (employee) {
+			const color = employee.color
+			if (!result[color]) {
+				result[color] = []
+			}
+			result[color].push(point)
+		}
+
+		return result
+	}, {} as GroupedMapPoints)
+}
