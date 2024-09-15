0edf24916e0013dfee8d860f9e96444a805221d7
Matias Agustin Area <matuarea@gmail.com>
2024-09-15 14:12:31 -0300

    Add MapComponent for displaying grouped map points with custom markers

---
+++ src/components/Map/MapComponent.tsx
@@ -0,0 +1,65 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+import L from 'leaflet'
+import { MapPoint } from '../../types/MapPoint.types'
+
+type GroupedMapPoints = {
+	[color: string]: MapPoint[]
+}
+
+interface MapComponentProps {
+	points: GroupedMapPoints
+}
+const MapComponent: React.FC<MapComponentProps> = ({ points }) => {
+	const markers = Object.keys(points).map((color) => {
+		const customIcon = new L.Icon({
+			iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+			shadowUrl:
+				'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+			iconSize: [25, 41],
+			iconAnchor: [12, 41],
+			popupAnchor: [1, -34],
+			shadowSize: [41, 41],
+		})
+
+		return points[color].map((point) => {
+			return (
+				<Marker
+					key={point.id}
+					position={[point.lat, point.lng]}
+					icon={customIcon}
+				>
+					<Popup>
+						<span>{point.name}</span>
+					</Popup>
+				</Marker>
+			)
+		})
+	})
+
+	// const customIcon = new L.Icon({
+	// 	iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${markerColor}.png`,
+	// 	shadowUrl:
+	// 		'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+	// 	iconSize: [25, 41],
+	// 	iconAnchor: [12, 41],
+	// 	popupAnchor: [1, -34],
+	// 	shadowSize: [41, 41],
+	// })
+	return (
+		<MapContainer
+			center={[-34.92145, -57.95453]}
+			zoom={13}
+			scrollWheelZoom={false}
+			style={{ height: '100%', width: '100%' }}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			{markers}
+		</MapContainer>
+	)
+}
+
+export default MapComponent
