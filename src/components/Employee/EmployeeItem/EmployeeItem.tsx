ef155c9409a5283cd9f77ea913d3c4b625b6e388
Matias Agustin Area <matuarea@gmail.com>
2024-09-15 14:12:04 -0300

    Refactor EmployeeComponent to accept employees as props; update EmployeeItem styling and layout

--- src/components/Employee/EmployeeItem/EmployeeItem.tsx
+++ src/components/Employee/EmployeeItem/EmployeeItem.tsx
@@ -6,12 +6,12 @@ interface EmployeeItemProps {
 
 export const EmployeeItem: React.FC<EmployeeItemProps> = ({ person }) => {
 	return (
-		<li key={person.id} className="flex justify-between gap-x-6 py-5">
+		<li key={person.id} className="flex justify-between gap-x-6 p-4">
 			<div className="flex min-w-0 gap-x-4">
 				<img
 					alt=""
 					src={person.imageUrl}
-					className="h-10 w-10 flex-none rounded-full bg-gray-50"
+					className="hidden h-10 w-10 flex-none rounded-full bg-gray-50 md:block"
 				/>
 				<div className="min-w-0 flex-auto">
 					<p className="text-sm font-semibold leading-6 text-gray-900">
@@ -19,8 +19,15 @@ export const EmployeeItem: React.FC<EmployeeItemProps> = ({ person }) => {
 					</p>
 				</div>
 			</div>
-			<div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-				<p className="text-sm leading-6 text-gray-900">{person.color}</p>
+			<div className="shrink-0 flex flex-col items-end mt-2">
+				<div
+					style={{
+						backgroundColor: person.color,
+						width: '0.75rem',
+						height: '0.75rem',
+						borderRadius: '50%',
+					}}
+				></div>
 			</div>
 		</li>
 	)
