6d1c99bb8cfa93f9a1331cfff2c483734e6a05fe
Matias Agustin Area <matuarea@gmail.com>
2024-09-16 11:00:24 -0300

    Refactor file paths for favicon and logo images

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => (
 	<header className="bg-white shadow-md rounded-b-lg sticky top-0 z-50">
 		<div className="container mx-auto px-4 py-4 flex justify-between items-center min-w-full">
-			<img src="/public/logo.png" alt="Logo" className="w-46 h-8" />
+			<img src="/logo.png" alt="Logo" className="w-46 h-8" />
 			<h1 className="hidden md:block text-2xl font-bold text-primary">
 				Zippin Map
 			</h1>
