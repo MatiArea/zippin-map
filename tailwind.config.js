dd7921b40f09bdc866cc6b28c97a42a29dcd862f
Matias Agustin Area <matuarea@gmail.com>
2024-09-16 10:25:54 -0300

    Ignore .env file and update tailwind.config.js to extend colors with zippin color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				zippin: '#49E8A8',
+			},
+		},
 	},
 	plugins: [],
 }
