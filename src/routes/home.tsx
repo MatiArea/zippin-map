dff1aea32dfaec94071b17b3bd0b3475b8a2fa44
Matias Agustin Area <matuarea@gmail.com>
2024-09-16 10:28:37 -0300

    Refactor Home component to handle employee selection and assignment

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,4 +1,3 @@
-import { createColorArray, groupMapPointsByColor } from '../utils/functions'
 import { useEffect, useState } from 'react'
 
 import { Employee } from '../types/Employee.types'
@@ -7,11 +6,19 @@ import EmployeeService from '../services/EmployeeService'
 import MapComponent from '../components/Map/MapComponent'
 import { MapPoint } from '../types/MapPoint.types'
 import MapPointService from '../services/MapPointService'
+import SelectModalComponent from '../components/Modal/SelectModalComponent'
 import TableComponent from '../components/Table/TableComponent'
+import { groupMapPointsByColor } from '../utils/functions'
 
 export const Home: React.FC = () => {
 	const [employees, setEmployees] = useState<Employee[]>([])
 	const [mapPoints, setMapPoints] = useState<MapPoint[]>([])
+	const [selectedMapPoint, setSelectedMapPoint] = useState<MapPoint[] | []>([])
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const [fetchData, setFetchData] = useState(false)
+	const [selectedEmployee, setSelectedEmployee] = useState<Employee | null>(
+		null,
+	)
 
 	useEffect(() => {
 		try {
@@ -29,27 +36,63 @@ export const Home: React.FC = () => {
 		} catch (e) {
 			console.error(e)
 		}
-	}, [])
+	}, [fetchData])
 
-	const colorArray = createColorArray(employees)
-	const mapPointsByColor = groupMapPointsByColor(mapPoints, employees)
+	const handleTableRowClick = (mapPoint: MapPoint[]) => {
+		setSelectedMapPoint(mapPoint)
+		setIsModalOpen(true)
+	}
 
-	console.log('COLOR ARRAY', colorArray)
-	console.log('MAP POINT', mapPointsByColor)
+	const handleEmployeeAssign = (employee: Employee | null) => {
+		if (selectedMapPoint) {
+			selectedMapPoint.forEach(async (point) => {
+				await MapPointService.updatePoint(point.id, employee ? employee.id : -1)
+			})
+		}
+
+		setSelectedMapPoint([]) // Reinicia la selección
+		setIsModalOpen(false)
+		setFetchData(!fetchData)
+	}
+
+	const handleEmployeeFilter = (employee: Employee | null) => {
+		setSelectedEmployee(employee)
+	}
+
+	const filteredMapPoints = selectedEmployee
+		? mapPoints.filter(
+				(point) => point.assignedDriverId === selectedEmployee.id,
+		  )
+		: mapPoints
+
+	const mapPointsByColor = groupMapPointsByColor(filteredMapPoints, employees)
 
 	return (
 		<div className="p-4">
 			<div className="flex flex-wrap-reverse justify-between items-stretch w-full">
 				<div className="w-full md:w-1/6 mb-4">
-					<EmployeeComponent employees={employees} />
+					<EmployeeComponent
+						employees={employees}
+						selected={selectedEmployee}
+						onEmployeeClick={handleEmployeeFilter}
+					/>
 				</div>
 				<div className="w-full md:w-4/5 h-96 mb-4">
 					<MapComponent points={mapPointsByColor} />
 				</div>
 			</div>
-			<div className="flex flex-col justify-center items-center  w-full py-4">
-				<TableComponent data={mapPoints} />
+			<div className="flex flex-col justify-center items-center w-full py-4">
+				<TableComponent data={mapPoints} onRowClick={handleTableRowClick} />
 			</div>
+			{isModalOpen && selectedMapPoint && (
+				<SelectModalComponent
+					employees={employees}
+					isOpen={isModalOpen}
+					onConfirm={handleEmployeeAssign}
+					onClose={() => setIsModalOpen(false)}
+					title="Asignar empleado"
+				/>
+			)}
 		</div>
 	)
 }
